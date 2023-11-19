b71c0c94431895220c4c2714d202e779828df0c6
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-11-18 19:51:16 -0700

    <Build>[optional_scope]: <Modal functionality add delete button and begin styling>
    [optional_body]
    
    [optional_footer]

--- main.js
+++ main.js
@@ -6,58 +6,103 @@ function Book(title, author, genre, status){
     this.genre = genre;
     this.status = status;
 }
-const grapesOfWrath = new Book('The Grapes of Wrath', 'John Steinbeck' , 'Fiction', 'Read');
-myLibrary.push(grapesOfWrath);
 
-function createBookModal(){
+function submitForm(e){
+    e.preventDefault();
+
+    const bookForm = document.getElementById('book-form');
+    const bookData = new FormData(bookForm);
+    const newBook = {
+         title : bookData.get('title'),
+         author : bookData.get('author'),
+         genre : bookData.get('genre'),
+         status : bookData.get('status'),
+    }
+    myLibrary.push(newBook);
+    addBookToSidebar(myLibrary.length-1);
+    bookForm.reset();
+}
+
+function addBookToSidebar(num){
+    const bookList = document.querySelector('.book-list');
+    const newBook = document.createElement('li');
+    const bookLink = document.createElement('a');
+    bookLink.href = '#';
+    bookLink.textContent = myLibrary[num].title;
+    
+    bookLink.addEventListener('click', function () {
+        createBookModal(num);
+    });
+    
+    newBook.append(bookLink);
+    bookList.appendChild(newBook);
+}
+
+
+function createBookModal(index){
     let modalBody = document.createElement('div');
     let modalText = document.createElement('p');
     let closeButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
 
     closeButton.textContent='X';
     closeButton.classList.add('close-button');
-    closeButton.addEventListener('click', function(){
-        modalBody.style.display = 'none';
-    })
+    closeButton.addEventListener('click', closeModal)
 
-    modalText.innerHTML = displayBookInfo(0);
+    deleteButton.textContent = 'Remove Book';
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', deleteBook);
+    deleteButton.addEventListener('click', closeModal)
+
+    modalText.innerHTML = displayBookInfo(index);
     modalBody.classList.add('modal');
 
     modalBody.append(closeButton);
     modalBody.append(modalText);
+    modalBody.append(deleteButton);
     document.body.append(modalBody);
 }
-function displayBookInfo(i){
+
+function closeModal(){
+    let modalBody = document.querySelector('.modal');
+    modalBody.style.display = 'none';
+}
+
+
+function displayBookInfo(index){
     return `
             <div>
-                <p class='modal-text'>Title: ${myLibrary[i].title}</p>
-                <p class='modal-text'>Author: ${myLibrary[i].author}</p>
-                <p class='modal-text'>Genre: ${myLibrary[i].genre}</p>
-                <p class='modal-text'>Status: ${myLibrary[i].status}</p>
+                <p class='modal-text'>Title: ${myLibrary[index].title}</p>
+                <p class='modal-text'>Author: ${myLibrary[index].author}</p>
+                <p class='modal-text'>Genre: ${myLibrary[index].genre}</p>
+                <p class='modal-text'>Status: ${myLibrary[index].status}</p>
             </div>
             `
 }
 
-function openModal(){
-    createBookModal()
+function deleteBook(index){
+    myLibrary.splice(index, 1);
+    updateBookList();
 }
 
-function addBookToLibrary(){
-const bookList = document.querySelector('.book-list'); ///targets ul
+function updateBookList(){
+    const bookList = document.querySelector('.book-list');
+    bookList.innerHTML = '';
 
-for(let i = 0; i < myLibrary.length; i++){
-    const newBook = document.createElement('li');
-    const bookLink = document.createElement('a');
-    bookLink.href = '#';
-    bookLink.onclick = openModal;
-
-    bookLink.innerHTML = myLibrary[i].title;
-    newBook.append(bookLink)
-    bookList.appendChild(newBook);
-    console.log(myLibrary[i])
+    for(let i = 0; i < myLibrary.length; i++){
+        addBookToSidebar(i)
     }
 }
 
+function toggleReadStatus(num){
+    if(myLibrary[num].status === 'Read'){
+        myLibrary[num].status = 'Unread';
+    } else {
+        myLibrary[num].status = 'Read';
+    }
+    closeModal();
+    createBookModal(num);
+}
 
-
-addBookToLibrary();
+const bookForm = document.getElementById('book-form');
+bookForm.addEventListener('submit', submitForm);
